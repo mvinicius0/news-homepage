2288ec50eecb419e2fcdf98c79ed49c669da66b2
Marcos Vinicius <marcosv.souza@telefonicati.onmicrosoft.com>
2025-02-24 10:00:09 -0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <HeaderComp />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
-
+import HeaderComp from "./components/HeaderComp.vue";
 @Options({
   components: {
-    HelloWorld,
+    HeaderComp,
   },
 })
 export default class App extends Vue {}
